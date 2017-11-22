54bf89129e910128fb0c72387bface5e16625370
Chris Simpson <chris.simpson@onthebeach.co.uk>
2017-11-22 14:52:32 +0000

    Add transitions/styles to prototype

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,3 +1,65 @@
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();
+    $('[data-toggle="popover"]').on('click', function(e) {
+      e.preventDefault();
+    });
+
+    var collapseAndShowNext = function(current, next) {
+      $(current).addClass('completed');
+      $(next).click();
+      toggleActive(next);
+    };
+
+    var toggleActive = function(element) {
+      $('.active').each(function() {
+        $(this).toggleClass('active');
+      });
+      $(element).toggleClass('active');
+    };
+
+    $('button[data-target="#customer-details-true"]').on('click', function() {
+      collapseAndShowNext($('a[href="#collapse1"]'), $('a[href="#collapse2"]'));
+      $('a[href="#collapse1"] .panel-title').html('Details OK');
+    });
+
+    $('#collapse2 input[name="optradio"]').on('click', function(event) {
+      collapseAndShowNext($('a[href="#collapse2"]'), $('a[href="#collapse3"]'));
+      if(event.target.dataset.target === '#insurance-details-true') {
+        $('a[href="#collapse2"] .panel-title').html('Insurance OK');
+      } else {
+        $('a[href="#collapse2"] .panel-title').html('Insurance added');
+      }
+    });
+
+    $('#collapse3 input[name="optradio"]').on('click', function() {
+      collapseAndShowNext($('a[href="#collapse3"]'), $('a[href="#collapse4"]'));
+      if(event.target.dataset.target === '#parking-details-true') {
+        $('a[href="#collapse3"] .panel-title').html('Parking OK');
+      } else {
+        $('a[href="#collapse3"] .panel-title').html('Airport parking added');
+      }
+    });
+
+    $('button[data-target="#customer-flights-true"]').on('click', function() {
+      collapseAndShowNext($('a[href="#collapse4"]'), $('a[href="#collapse5"]'));
+      $('a[href="#collapse4"] .panel-title').html('Flights OK');
+    });
+
+    $('button[data-target="#customer-hotel-true"]').on('click', function() {
+      collapseAndShowNext($('a[href="#collapse5"]'), $('a[href="#collapse6"]'));
+      $('a[href="#collapse5"] .panel-title').html('Hotel OK');
+    });
+
+    $('#collapse6 input[name="optradio"]').on('click', function() {
+      collapseAndShowNext($('a[href="#collapse6"]'), $('a[href="#collapse6"]'));
+      if(event.target.dataset.target === '#customer-transfers-true') {
+        $('a[href="#collapse6"] .panel-title').html('Transfer OK');
+      } else {
+        $('a[href="#collapse6"] .panel-title').html('Transfer added');
+      }
+    });
+
+    $('a').on('click', function() {
+      toggleActive($(this));
+    });
 });
